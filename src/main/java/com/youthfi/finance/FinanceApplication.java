859aa17ce974876374eb8ea3f1b7cc3854054152
ydking0911 <ydking0911@gmail.com>
2025-09-09 12:59:46 +0900

    [FEAT] Global Setting

--- src/main/java/com/youthfi/finance/FinanceApplication.java
+++ src/main/java/com/youthfi/finance/FinanceApplication.java
@@ -2,8 +2,12 @@ package com.youthfi.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableConfigurationProperties
 public class FinanceApplication {
 
 	public static void main(String[] args) {
