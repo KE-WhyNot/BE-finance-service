8dee494573ad528476712150f658a161031bac00
ydking0911 <ydking0911@gmail.com>
2025-09-09 10:41:12 +0900

    [FEAT] Initial Commit

---
+++ src/test/java/com/youthfi/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youthfi.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
